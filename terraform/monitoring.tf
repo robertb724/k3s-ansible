cd1186fe9f3acbdbe9b144d6a9f751ebfcbd935f
Robert Batson <krymsontide724@gmail.com>
2024-02-22 23:33:41 -0500

    enabled pvc for prometheus

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -6,6 +6,7 @@ resource "helm_release" "kube-prometheus-stack" {
   create_namespace = true
   #   version          = "4.9.1"
   values = [
-    file("${path.module}/values/grafana.values.yaml")
+    file("${path.module}/values/grafana.values.yaml"),
+    file("${path.module}/values/prometheus.values.yaml")
   ]
 }
